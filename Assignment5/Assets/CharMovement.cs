ec64d01fe3ad868df838d4cca99d001b4f8cd6dc
Johachim <joachimgul@yahoo.dk>
2019-09-03 11:27:25 +0200

    Kamera, collision og hop mangler

---
+++ Assignment5/Assets/CharMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float speed = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x_auto = Time.deltaTime * speed;
+        transform.Translate(x_auto, 0, 0);
+
+    }
+}
